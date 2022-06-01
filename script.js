db875ae0b976825f1fc5b43498ef12b2cb80f26a
sree-lakshmi-n <sreelakshmin153@gmai.com>
2022-06-01 16:28:59 +0530

    Weekly distribution spending statistics elements added through script.

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 `use strict`;
+const spending = document.getElementsByClassName(
+  "spending-weekly-distribution"
+)[0];
+const days = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
+days.forEach((day) => {
+  const spendingDiv = document.createElement("div");
+  spendingDiv.setAttribute("class", `${day}-spending`);
+  const label = document.createElement("p");
+  label.setAttribute("class", `${day}-spending-label`);
+  spending.append(spendingDiv);
+  spending.append(label);
+});
